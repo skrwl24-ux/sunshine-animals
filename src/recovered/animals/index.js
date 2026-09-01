7e301d2a809e1ba63f95f0518c0c5bad7b543a35
skrwl24-ux <skrwl24@gmail.com>
2026-09-01 12:25:31 +0900

    Merge recovered animals with main game expansion data

--- src/recovered/animals/index.js
+++ src/recovered/animals/index.js
@@ -3,14 +3,19 @@ import { animals as rare } from './rare.js';
 import { animals as epic } from './epic.js';
 import { animals as legendary } from './legendary.js';
 import { animals as mythic } from './mythic.js';
+import { legacyAnimalExpansion } from '../legacy-main-adapter.js';
 
-export const animals = [...common, ...rare, ...epic, ...legendary, ...mythic];
+export const recoveredAnimals = [...common, ...rare, ...epic, ...legendary, ...mythic];
+export const expansionAnimals = legacyAnimalExpansion(recoveredAnimals);
+export const animals = [...recoveredAnimals, ...expansionAnimals];
 
 export const animalCounts = Object.freeze({
   total: animals.length,
-  common: common.length,
-  rare: rare.length,
-  epic: epic.length,
-  legendary: legendary.length,
-  mythic: mythic.length,
+  recovered: recoveredAnimals.length,
+  expansion: expansionAnimals.length,
+  common: animals.filter((animal) => animal.rarity === '일반').length,
+  rare: animals.filter((animal) => animal.rarity === '희귀').length,
+  epic: animals.filter((animal) => animal.rarity === '특별').length,
+  legendary: animals.filter((animal) => animal.rarity === '전설').length,
+  mythic: animals.filter((animal) => animal.rarity === '환상').length,
 });
