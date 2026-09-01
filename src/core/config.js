bcaa80c8e729c31d82fcccb4fb515eef2c9f0199
skrwl24-ux <skrwl24@gmail.com>
2026-09-01 11:45:36 +0900

    Align core config with recovered live deployment storage schema

--- src/core/config.js
+++ src/core/config.js
@@ -1,21 +1,35 @@
 export const STORAGE_KEYS = Object.freeze({
-  light: 'saLight',
-  dust: 'saDust',
-  owned: 'saOwned',
-  language: 'saLang',
-  imageCache: 'saImgCache',
+  progress: 'animal-discovery-v3',
+  language: 'sunshine-language',
+  partyNickname: 'sunshine-party-nickname',
+  partyPlayer: 'sunshine-party-player',
+});
+
+export const DEFAULT_PROGRESS = Object.freeze({
+  light: 5,
+  owned: Object.freeze({ capybara: 1 }),
+  behaviors: Object.freeze({ 'capy-rest': true }),
+  traces: Object.freeze({}),
+  quizWins: 0,
+  explorations: 0,
+  partyGames: 0,
+  partyBest: 0,
+  claimed: Object.freeze({}),
 });
 
 export const DEFAULTS = Object.freeze({
-  light: 53,
-  dust: 0,
   language: 'ko',
+  progress: DEFAULT_PROGRESS,
 });
 
 export const EGG_COST = Object.freeze({
   forest: 5,
   radiant: 10,
 });
+
+export const PARTY_LIGHT_REWARD_SCORE = 200;
+export const RESCUE_LIGHT_REWARD_SCORE = 500;
+export const RESCUE_DURATION_SECONDS = 30;
 
 export const LANGUAGES = Object.freeze(['ko', 'en']);
 
